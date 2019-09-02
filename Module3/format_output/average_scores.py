e752477face1d52b960ff9efbb33e811edbaf8cf
Greg Tarr <gregory.a.tarr@gmail.com>
2019-09-02 17:51:41 -0500

    Completed Function

--- Module3/format_output/average_scores.py
+++ Module3/format_output/average_scores.py
@@ -1,6 +1,10 @@
 
 def average():
-    pass
+    score_one = input("Enter first score: ")
+    score_two = input("Enter second score: ")
+    score_three = input("Enter third score: ")
+    average = (score_one + score_two + score_three) / 3
+    return average
 
 
 if __name__ == '__main__':
