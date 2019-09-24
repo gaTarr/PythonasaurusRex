13e691a1882f86e53584e07a9595d2960d944f61
Greg Tarr <gregory.a.tarr@gmail.com>
2019-09-23 22:48:34 -0500

    Module_4_Topic_4 Input validation
    Initial Commit, failing tests

---
+++ Module_4/input_validation/validation_with_if.py
@@ -0,0 +1,12 @@
+"""CIS189 Python
+Author: Greg Tarr
+Created: 09/23/2019"""
+
+
+def average(score1, score2, score3):
+    number_tests = 3
+    return float((score1 + score2 + score3)/number_tests)
+
+
+if __name__ == '__main__':
+    pass
---
+++ Module_4/test/test_validation_with_if.py
@@ -0,0 +1,17 @@
+"""CIS189 Python
+Author: Greg Tarr
+Created: 09/23/2019"""
+
+
+import unittest
+from input_validation import validation_with_if
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_input_validation_if(self):
+        self.assertEqual(-1, validation_with_if.average(-90, 90, 95))
+
+
+if __name__ == '__main__':
+    unittest.main()
