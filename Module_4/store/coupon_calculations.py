07c400b5d8dad8a3b27a311e3b330d4b468007a3
Greg Tarr <gregory.a.tarr@gmail.com>
2019-09-23 21:29:51 -0500

    Nested If Statement Assignment Commit 2
    Added logic for calculating a price under $10.  All tests pass.

--- Module_4/store/coupon_calculations.py
+++ Module_4/store/coupon_calculations.py
@@ -3,8 +3,31 @@ Author: Greg Tarr
 Created: 09/23/2019"""
 
 
+def calculate_tax(subtotal):
+    tax = .06
+    return subtotal + (subtotal * tax)
+
+
+def calculate_subtotal(price, cash, percent):
+    return (price - cash) - ((price - cash) * percent)
+
+
 def calculate_order(price, cash_coupon, percent_coupon):
-    pass
+    if price < 10:
+        if cash_coupon == 5:
+            if percent_coupon == .1:
+                return round((calculate_tax(calculate_subtotal(price, cash_coupon, percent_coupon)) + 5.95), 2)
+            elif percent_coupon == .15:
+                return round((calculate_tax(calculate_subtotal(price, cash_coupon, percent_coupon)) + 5.95), 2)
+            elif percent_coupon == .2:
+                return round((calculate_tax(calculate_subtotal(price, cash_coupon, percent_coupon)) + 5.95), 2)
+            else:
+                print("Invalid coupon Percentage.")
+        elif cash_coupon == 10:
+            if percent_coupon == .1 or percent_coupon == .15 or percent_coupon == .2:
+                return 5.95
+            else:
+                print("Invalid coupon Percentage.")
 
 
 if __name__ == '__main__':
