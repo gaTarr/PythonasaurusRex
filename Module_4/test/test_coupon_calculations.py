5fca78c50168142cd96c716ab2acb4dff2eebc04
Greg Tarr <gregory.a.tarr@gmail.com>
2019-09-23 21:46:04 -0500

    Nested If Statement Assignment Commit 3
    Failing after adding more tests

--- Module_4/test/test_coupon_calculations.py
+++ Module_4/test/test_coupon_calculations.py
@@ -16,5 +16,14 @@ class MyTestCase(unittest.TestCase):
         assert coupon_calculations.calculate_order(9, 10, .2) == 5.95
 
 
+    def test_price_under_between_ten_thirty(self):
+        assert coupon_calculations.calculate_order(15.99, 5, .1) == 18.43
+        assert coupon_calculations.calculate_order(15.99, 5, .15) == 17.85
+        assert coupon_calculations.calculate_order(15.99, 5, .2) == 17.27
+        assert coupon_calculations.calculate_order(15.99, 10, .1) == 13.66
+        assert coupon_calculations.calculate_order(15.99, 10, .15) == 13.35
+        assert coupon_calculations.calculate_order(15.99, 10, .2) == 13.03
+
+
 if __name__ == '__main__':
     unittest.main()
