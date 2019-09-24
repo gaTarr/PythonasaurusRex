4c0535a4a53bb16ee84132e9ec5d3f3542ee4001
Greg Tarr <gregory.a.tarr@gmail.com>
2019-09-23 22:57:05 -0500

    Module_4_Topic_4
    Final input validation.  Modified logic to handle negative values for each of/all scores.  Tests pass

--- Module_4/input_validation/validation_with_if.py
+++ Module_4/input_validation/validation_with_if.py
@@ -5,7 +5,7 @@ Created: 09/23/2019"""
 
 def average(score1, score2, score3):
     number_tests = 3
-    if score1 < 0:
+    if score1 < 0 or score2 < 0 or score3 < 0:
         return -1
     else:
         return float((score1 + score2 + score3)/number_tests)
--- Module_4/test/test_validation_with_if.py
+++ Module_4/test/test_validation_with_if.py
@@ -11,6 +11,9 @@ class MyTestCase(unittest.TestCase):
 
     def test_input_validation_if(self):
         self.assertEqual(-1, validation_with_if.average(-90, 90, 95))
+        self.assertEqual(-1, validation_with_if.average(90, -92, 95))
+        self.assertEqual(-1, validation_with_if.average(90, 90, -95))
+        self.assertEqual(-1, validation_with_if.average(-90, -90, -95))
 
 
 if __name__ == '__main__':
