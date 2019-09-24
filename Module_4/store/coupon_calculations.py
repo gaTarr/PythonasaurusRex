588364e6095a512c59f8afd2323357765b74a079
Greg Tarr <gregory.a.tarr@gmail.com>
2019-09-23 21:52:34 -0500

    Nested If Statement Assignment Commit 4
    Adding logic to accept price input > 10 & < 30.  Tests pass

--- Module_4/store/coupon_calculations.py
+++ Module_4/store/coupon_calculations.py
@@ -28,6 +28,25 @@ def calculate_order(price, cash_coupon, percent_coupon):
                 return 5.95
             else:
                 print("Invalid coupon Percentage.")
+    elif 10 < price < 30:
+        if cash_coupon == 5:
+            if percent_coupon == .1:
+                return round((calculate_tax(calculate_subtotal(price, cash_coupon, percent_coupon)) + 7.95), 2)
+            elif percent_coupon == .15:
+                return round((calculate_tax(calculate_subtotal(price, cash_coupon, percent_coupon)) + 7.95), 2)
+            elif percent_coupon == .2:
+                return round((calculate_tax(calculate_subtotal(price, cash_coupon, percent_coupon)) + 7.95), 2)
+            else:
+                print("Invalid coupon Percentage.")
+        elif cash_coupon == 10:
+            if percent_coupon == .1:
+                return round((calculate_tax(calculate_subtotal(price, cash_coupon, percent_coupon)) + 7.95), 2)
+            elif percent_coupon == .15:
+                return round((calculate_tax(calculate_subtotal(price, cash_coupon, percent_coupon)) + 7.95), 2)
+            elif percent_coupon == .2:
+                return round((calculate_tax(calculate_subtotal(price, cash_coupon, percent_coupon)) + 7.95), 2)
+            else:
+                print("Invalid coupon Percentage.")
 
 
 if __name__ == '__main__':
