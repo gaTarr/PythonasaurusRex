2a55014163ba21520472e42abda3d0ab82af840c
Greg Tarr <gregory.a.tarr@gmail.com>
2019-09-24 21:21:13 -0500

    Final update with Try
    Okay, I know I got a little out of turn here, but I got so excited with my or statements, I wrote the logic to account for if each of the values were negative.  So, I'm here adding the testing for the 3rd score after writing the logic (not TDD, I know).
    
    Also, I added a test to try all 3 negative, and it still passes.

--- Module_4/test/test_validation_with_try.py
+++ Module_4/test/test_validation_with_try.py
@@ -14,7 +14,15 @@ class MyTestCase(unittest.TestCase):
 
     def test_average_exception(self):
         with self.assertRaises(ValueError):
-            valid_try.average(90,- 89, 78)
+            valid_try.average(90, -89, 78)
+
+    def test_average_negative_three(self):
+        with self.assertRaises(ValueError):
+            valid_try.average(90, 89, -78)
+
+    def test_average_negative_all(self):
+        with self.assertRaises(ValueError):
+            valid_try.average(-90, -89, -78)
 
 
 if __name__ == '__main__':
