a7be313c994224ca20bf77ffc82fa8631031a557
Greg Tarr <gregory.a.tarr@gmail.com>
2019-09-24 21:10:34 -0500

    First Input Value error
    test passes

--- Module_4/input_validation/validation_with_try.py
+++ Module_4/input_validation/validation_with_try.py
@@ -5,8 +5,16 @@ Created: 09/23/2019"""
 
 def average(score1, score2, score3):
     number_tests = 3
+
+    if score1 < 0:
+        raise ValueError
+
     return float((score1 + score2 + score3)/number_tests)
 
 
 if __name__ == '__main__':
-    pass
+
+    try:
+        average(-10, 12, 14)
+    except ValueError as err:
+        print("Bad input!")
--- Module_4/test/test_validation_with_try.py
+++ Module_4/test/test_validation_with_try.py
@@ -7,9 +7,10 @@ from input_validation import validation_with_try as valid_try
 
 
 class MyTestCase(unittest.TestCase):
+
     def test_average_negative_input(self):
         with self.assertRaises(ValueError):
-            valid_try.average(90, - 89, 78)
+            valid_try.average(-90, 89, 78)
 
 
 if __name__ == '__main__':
