29ee38240058a5ef72255672a5045233cee9f0b2
Greg Tarr <gregory.a.tarr@gmail.com>
2019-09-26 20:37:25 -0500

    Module5 Topic2 Commit 3
    Completed program, tests pass

--- Module5/input_loops/average_scores.py
+++ Module5/input_loops/average_scores.py
@@ -14,4 +14,41 @@ def average(score_list):
 
 
 if __name__ == '__main__':
-    pass
+    last_name = input('Please enter your last name: ')
+    first_name = input('Please enter your last name: ')
+    scores = []
+    score = 0
+    message = "Please enter a score, or -1 to stop "
+    while score != -1:
+        score = float(input(message))
+        if score == -1:
+            break
+        scores.append(score)
+
+    avg_scores = average(scores)
+    print(last_name + ', ' + first_name + " grade: %5.2f" % avg_scores)
+
+
+# Run One:
+# Please enter your last name: tarr
+# Please enter your last name: greg
+# Please enter a score, or -1 to stop 33
+# Please enter a score, or -1 to stop 44
+# Please enter a score, or -1 to stop 55
+# Please enter a score, or -1 to stop -1
+# tarr, greg grade: 44.00
+#
+# Process finished with exit code 0
+
+# Run Two:
+# Please enter your last name: Tarr
+# Please enter your last name: Greg
+# Please enter a score, or -1 to stop 33
+# Please enter a score, or -1 to stop 5
+# Please enter a score, or -1 to stop 1
+# Please enter a score, or -1 to stop 0
+# Please enter a score, or -1 to stop 99
+# Please enter a score, or -1 to stop -1
+# Tarr, Greg grade: 27.60
+#
+# Process finished with exit code 0
