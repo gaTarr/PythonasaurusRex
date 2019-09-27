227f965092e00481ebf957a365afc22d96d2f727
Greg Tarr <gregory.a.tarr@gmail.com>
2019-09-26 20:12:53 -0500

    Module5 Topic2 Commit2
    Added logic to average() function, test passes

--- Module5/input_loops/average_scores.py
+++ Module5/input_loops/average_scores.py
@@ -4,7 +4,13 @@ Created: 09/26/2019"""
 
 
 def average(score_list):
-    pass
+    counter = 0
+    total = 0
+    for score in score_list:
+        total = total + score
+        counter += 1
+
+    return total / counter
 
 
 if __name__ == '__main__':
