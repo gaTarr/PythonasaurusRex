e97f5f9754b1c66b5637d17bda210eb58e94219a
Greg Tarr <gregory.a.tarr@gmail.com>
2019-10-08 21:09:26 -0500

    tests written for sort_list()
    tests failing

--- Module7/test_collections/test_sort_and_search_list.py
+++ Module7/test_collections/test_sort_and_search_list.py
@@ -10,6 +10,11 @@ class MyTestCase(unittest.TestCase):
     def test_search_item_not_found(self):
         self.assertEqual(sort_and_search_list.search_list([8, 11, 5], 7), -1)
 
+    def test_sort_list_sorted(self):
+        test_input = [8, 11, 5]
+        expected_result = [5, 8, 11]
+        self.assertEqual(sort_and_search_list.sort_list(test_input), expected_result)
+
 
 if __name__ == '__main__':
     unittest.main()
