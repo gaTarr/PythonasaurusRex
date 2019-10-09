2de4263c8f8283ebbceac6504952e6e816f33c46
Greg Tarr <gregory.a.tarr@gmail.com>
2019-10-08 21:24:38 -0500

    Completed logic for ssort_list()
    tests pass.
    
    ALSO: included function docstring

--- Module7/fun_with_collections/basic_list_exception.py
+++ Module7/fun_with_collections/basic_list_exception.py
@@ -4,6 +4,9 @@ Created: 10/07/2019"""
 
 
 def make_list():
+    """ This gets user input, evaluates the input and inserts it into a list
+    :returns: an error if invalid input is entered, or the list of input.
+    """
     number_list = []
     for number in range(3):
         try:
@@ -18,6 +21,9 @@ def make_list():
 
 
 def get_input():
+    """ This prompts user for input, and returns the input
+    :returns: an integer, if the user input is valid.
+    """
     try:
         user_input = int(input("Enter a number: "))
     except ValueError as err:
--- Module7/fun_with_collections/sort_and_search_list.py
+++ Module7/fun_with_collections/sort_and_search_list.py
@@ -5,19 +5,31 @@ from fun_with_collections import basic_list_exception
 
 
 def sort_list(a_list):
-    pass
+    """ This takes a list and sorts the values
+    :param a_list: this is the list which will be sorted
+    :returns: the list after it has been sorted"""
+    a_list.sort()
+    return a_list
 
 
 def search_list(a_list, a_value):
+    """ This takes a list and a value to search for, and if the value is found, returns it position
+    :param a_list: this represents the list which will be searched
+    :param a_value: this is the value that will be searched for
+    :returns: -1 if the value is not found or the index of the value if it is found
+    """
     try:
         a_list.index(a_value)
     except ValueError as err:
-        return -1
+        return -1                       # return was included here to gracefully handle value not found
     else:
-        return a_list.index(a_value)
+        return a_list.index(a_value)    # return to show user where value is
 
 
 if __name__ == '__main__':
+    test_list = [10, 33, 2, 4, 22]
+    # print(test_list)
+    print(sort_list(test_list))
     input_value = int(input("Enter an integer to search for: "))
     print(search_list(basic_list_exception.make_list(), input_value))
 
