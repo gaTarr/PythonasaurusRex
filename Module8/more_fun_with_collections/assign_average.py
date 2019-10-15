6f00da49b281b9177c0bf4e25c1713fb6bf05dae
Greg Tarr <gregory.a.tarr@gmail.com>
2019-10-14 22:45:41 -0500

    Selection Using Dictionary Assignment
    Tests written, failing

---
+++ Module8/more_fun_with_collections/assign_average.py
@@ -0,0 +1,11 @@
+"""CIS189 Python
+Author: Greg Tarr
+Created: 10/14/2019"""
+
+
+def switch_average():
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ Module8/test/test_assign_average.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest import mock
+
+from more_fun_with_collections.assign_average import switch_average
+
+
+class MyTestCase(unittest.TestCase):
+    def test_found_a(self):
+        with mock.patch('builtins.input', side_effect=['A']):
+            assert switch_average() == 100
+
+    def test_found_b(self):
+        with mock.patch('builtins.input', side_effect=['B']):
+            assert switch_average() == 200
+
+    def test_found_c(self):
+        with mock.patch('builtins.input', side_effect=['C']):
+            assert switch_average() == 300
+
+    def test_found_d(self):
+        with mock.patch('builtins.input', side_effect=['D']):
+            assert switch_average() == 400
+
+    def test_found_e(self):
+        with mock.patch('builtins.input', side_effect=['E']):
+            assert switch_average() == 'Invalid Input'
+
+
+if __name__ == '__main__':
+    unittest.main()
