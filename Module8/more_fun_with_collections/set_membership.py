9e2624356d2abd7f77ea1b711d3fbbbda63e64cb
Greg Tarr <gregory.a.tarr@gmail.com>
2019-10-14 20:30:20 -0500

    Sets
    Tests passing

--- Module8/more_fun_with_collections/set_membership.py
+++ Module8/more_fun_with_collections/set_membership.py
@@ -4,8 +4,15 @@ Created: 10/14/2019"""
 
 
 def in_set(a_set, a_value):
-    pass
+    """ This accepts a set and a value to examine
+    :param a_set: is the set that's passed in
+    :param a_value: is the value is going to be tested for in the set
+    :returns True if the value is in the set, False if the value is not in the set"""
+    return a_value in a_set
 
 
 if __name__ == '__main__':
-    pass
+    our_set = {1, 2, 3}
+    our_value = 3
+
+    print(in_set(our_set, our_value))
--- Module8/test/test_set_membership.py
+++ Module8/test/test_set_membership.py
@@ -4,12 +4,12 @@ from more_fun_with_collections import set_membership as sm
 
 class MyTestCase(unittest.TestCase):
     def test_in_set_true(self):
-        test_set = set('12345')
+        test_set = {1, 2, 3, 4, 5}
         test_value = 4
         self.assertTrue(sm.in_set(test_set, test_value))
 
     def test_in_set_false(self):
-        test_set = set('12345')
+        test_set = {1, 2, 3, 4, 5}
         test_value = 7
         self.assertFalse(sm.in_set(test_set, test_value))
 
