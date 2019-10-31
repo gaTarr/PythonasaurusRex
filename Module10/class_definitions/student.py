2a57ff94cc9409cc94df19d0a964ae8753b5ad73
Greg Tarr <gregory.a.tarr@gmail.com>
2019-10-30 21:34:01 -0500

    Student Class with Unit Tests Commit 1
    I didn't want to make several commits. So I've written all the tests, now I'll start working on getting them to pass.

---
+++ Module10/class_definitions/student.py
@@ -0,0 +1,17 @@
+"""CIS189 Python
+Author: Greg Tarr
+Created: 10/29/2019"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, lname, fname, major, gpa=0.0):
+        self.last_name = lname
+        self.first_name = fname
+        self.major = major
+        self.gpa = gpa
+
+    def __str__(self):
+        return self.last_name + ", " + self.first_name + " has major " + self.major + " with gpa: " + str(self.gpa)
+
+
---
+++ Module10/test/student_unit_tests.py
@@ -0,0 +1,45 @@
+import unittest
+from class_definitions import student as s
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.student = s.Student('Tarr', 'Greg', 'CIS')
+
+    def tearDown(self):
+        del self.student
+
+    def test_object_created_required_attributes(self):
+        self.assertEqual(self.student.last_name, 'Tarr')
+        self.assertEqual(self.student.first_name, 'Greg')
+        self.assertEqual(self.student.major, 'CIS')
+
+    def test_object_created_all_attributes(self):
+        student = s.Student('Tarr', 'Greg', 'CIS', 4.0)
+        assert student.last_name == 'Tarr'
+        assert student.first_name == 'Greg'
+        assert student.major == "CIS"
+        assert student.gpa == 4.0
+
+    def test_student_str(self):
+        self.assertEqual(str(self.student), 'Tarr, Greg has major CIS with gpa: 4.0')
+
+    def test_object_not_created_error_last_name(self):
+        with self.assertRaises(ValueError):
+            invalidStudet = s.Student('123', 'Greg', 'CIS')
+
+    def test_object_not_created_error_first_name(self):
+        with self.assertRaises(ValueError):
+            invalidStudent = s.Student('Tarr', '123', 'CIS')
+
+    def test_object_not_created_error_major(self):
+        with self.assertRaises(ValueError):
+            invalidStudent = s.Student('Tarr', 'Greg', '123')
+
+    def test_object_not_created_error_gpa(self):
+        with self.assertRaises(ValueError):
+            invalidStudent = s.Student('Tarr', 'Greg', 'CIS', '234')
+
+
+if __name__ == '__main__':
+    unittest.main()
